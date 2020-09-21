b949ef89aa3f474ca7f449ec92c536a35c6582ce
Auretyube-Git <auretyube@gmail.com>
2020-09-21 23:55:46 +0200

    SSD Commit

--- src/main/java/fr/auretyube/electronics/init/ElectronicsModItems.java
+++ src/main/java/fr/auretyube/electronics/init/ElectronicsModItems.java
@@ -17,10 +17,18 @@ public class ElectronicsModItems {
 	
 	public static final Item SILVER = new SilverItem(new Item.Properties().group(ElectronicsModItemGroup.INSTANCE)).setRegistryName(ElectronicsMod.MODID, "silver");
 	public static final Item COPPER = new CopperItem(new Item.Properties().group(ElectronicsModItemGroup.INSTANCE)).setRegistryName(ElectronicsMod.MODID, "copper");
+	public static final Item SILVER_WAFER = new Item(new Item.Properties().group(ElectronicsModItemGroup.INSTANCE)).setRegistryName(ElectronicsMod.MODID, "silver_wafer");
+	public static final Item COPPER_PLATE = new Item(new Item.Properties().group(ElectronicsModItemGroup.INSTANCE)).setRegistryName(ElectronicsMod.MODID, "copper_plate");
+	public static final Item PRINTED_CIRCUIT = new Item(new Item.Properties().group(ElectronicsModItemGroup.INSTANCE)).setRegistryName(ElectronicsMod.MODID, "printed_circuit");
+	public static final Item SSD = new Item(new Item.Properties().group(ElectronicsModItemGroup.INSTANCE)).setRegistryName(ElectronicsMod.MODID, "ssd");
 	
 	@SubscribeEvent
 	public static final void regsterItems(final RegistryEvent.Register<Item> event) {
 		event.getRegistry().register(SILVER);
 		event.getRegistry().register(COPPER);
+		event.getRegistry().register(SILVER_WAFER);
+		event.getRegistry().register(COPPER_PLATE);
+		event.getRegistry().register(PRINTED_CIRCUIT);
+		event.getRegistry().register(SSD);
 	}
 }
